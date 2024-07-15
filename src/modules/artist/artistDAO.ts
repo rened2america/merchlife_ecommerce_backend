8b0f30f6a5871cb0dafb6df25daca38a270d987a
Raj <raj@d2america.com>
2024-07-15 21:35:35 +0530

    Remove canvas and poster from seller products

--- src/modules/artist/artistDAO.ts
+++ src/modules/artist/artistDAO.ts
@@ -50,20 +50,42 @@ class ArtistDAO {
     limit: number
   ): Promise<{ artists: any[]; count: number } | null> => {
     console.log(page);
+    const ids:Array<{id:number}> = await prisma.$queryRaw`
+    SELECT "Artist"."id"
+    FROM "Artist" JOIN "Product" ON "Product"."artistId" = "Artist"."id"
+    JOIN "_ProductToType" ON "_ProductToType"."A" ="Product"."id"
+    JOIN "Type" ON "Type"."id" = "_ProductToType"."B"
+    WHERE "Type"."value" NOT IN ('Poster', 'Canvas')
+    GROUP BY "Artist"."id"
+    HAVING COUNT("Artist"."id") >= 3
+    ORDER BY RANDOM()
+    LIMIT 5;
+  `;
+    const artistIdArray = ids.map(artist => artist.id);
     const [artists, count] = await prisma.$transaction([
       prisma.artist.findMany({
         skip: (page - 1) * limit,
         take: limit,
-        include: {
+        where:{id:{in:artistIdArray}},
+        select: {
+          id: true,
+          email: true,
+          name: true,
+          banner:true,
           product: {
-            take: 3,
-            include: {
-              design: {
-                take: 1
-              }
-            }
-          }
-        }
+            take:3,
+            where: {
+              types: { some: { value: { notIn: ["Poster", "Canvas"] } } },
+            },
+            select: {
+              id: true,
+              title: true,
+              types: true,
+              description: true,
+              design:{take:1},
+            },
+          },
+        },
       }),
       prisma.artist.count(),
     ]);
@@ -86,6 +108,9 @@ class ArtistDAO {
           },
         },
         include: {
+          design: {
+            take:1
+          },
           types: true,
         },
       }),
